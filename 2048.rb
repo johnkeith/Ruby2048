e3afd287fe5a17f7574ea32b472aa93a8b07215e
John Keith <johnkeith@outlook.com>
2014-05-21 17:19:08 -0400

    Init commit
    Working game loop.
    
    Needs to have end/faliure conditions.
    
    Might also be cool to have a score method.

---
+++ 2048.rb
@@ -0,0 +1,164 @@
+require 'pry'
+# 2048 command line
+
+# create an array of arrays for game board - DONE
+
+# insert a 2 or a 4 into a random place on the board - DONE
+
+# ask for user input - a, s, w, or d 
+
+# move pieces on board
+
+# add new random 2 or 4 piece
+
+# break if you reach 2048
+
+
+game_board = [[" ", " ", " ", " "],
+              [" ", " ", " ", " "],
+              [" ", " ", " ", " "],
+              [" ", " ", " ", " "]]
+
+test_board = [["2", "2", " ", "4"],
+              ["2", " ", "4", " "],
+              [" ", "2", " ", " "],
+              [" ", "4", " ", " "]]
+
+# p test_board
+
+# p test_board.transpose
+
+# p test_board.transpose.transpose
+
+
+
+def main_game(start_board)
+  while true
+    # display board
+    start_board.each do |row|
+      puts row.join(" | ")
+    end
+    # ask for move
+    start_board=ask_for_move(start_board)
+    start_board = insert_new(start_board)
+  end
+end
+
+def insert_new(board)
+  successful = false
+  while !successful
+    random_position_one = rand(4)
+    random_position_two = rand(4)
+    two_or_four= rand(2) == 1 ? "2" : "4"
+    if board[random_position_one][random_position_two] == " "
+      board[random_position_one][random_position_two] = two_or_four
+      successful = true
+    end
+  end
+  board
+end
+
+def ask_for_move(board)
+  puts "Which way do you want to move? (Press 'A', 'S', 'D' or 'W')"
+  move = gets.chomp.upcase
+  case move
+    when "A" then board_after_move=move_left(board)
+    when "D" then board_after_move=move_right(board)
+    when "S" then board_after_move=move_down(board)
+    when "W" then board_after_move=move_up(board)
+    else ask_for_move(board)
+  end
+  board_after_move
+end
+
+# while there is an empty space in the row to the left
+def strip_row_left(row)
+  row.delete_if {|space| space == " "}
+  until row.length == 4
+    row.push(" ")
+  end
+  row
+end
+
+def strip_row_right(row)
+  row.delete_if {|space| space == " "}
+  until row.length == 4
+    row.unshift(" ")
+  end
+  row
+end
+
+def move_left(board)
+  board.each_with_index do |row, row_idx|
+    strip_row_left(row)
+    row.each_with_index do |column, col_idx|
+      if column == row[col_idx + 1] && !(column == " " )
+        board[row_idx][col_idx] = (board[row_idx][col_idx].to_i * 2).to_s
+        board[row_idx][col_idx+1] = " "
+      else
+        next
+      end
+    end
+    strip_row_left(row)
+  end
+  board
+end
+
+def move_right(board)
+  board.each_with_index do |row, row_idx|
+    strip_row_right(row)
+    row.reverse!.each_with_index do |column, col_idx|
+      if column == row[col_idx + 1] && !(column == " " )
+        board[row_idx][col_idx] = (board[row_idx][col_idx].to_i * 2).to_s
+        board[row_idx][col_idx+1] = " "
+      else
+        next
+      end
+    end
+    row.reverse!
+    strip_row_right(row)
+  end
+  board
+end
+
+def move_down(board)
+  board = board.transpose
+  board.each_with_index do |row, row_idx|
+    strip_row_right(row)
+    row.reverse!.each_with_index do |column, col_idx|
+      if column == row[col_idx + 1] && !(column == " " )
+        board[row_idx][col_idx] = (board[row_idx][col_idx].to_i * 2).to_s
+        board[row_idx][col_idx+1] = " "
+      else
+        next
+      end
+    end
+    row.reverse!
+    strip_row_right(row)
+  end
+  board.transpose
+end
+
+def move_up(board)
+  board = board.transpose
+  board.each_with_index do |row, row_idx|
+    strip_row_left(row)
+    row.each_with_index do |column, col_idx|
+      if column == row[col_idx + 1] && !(column == " " )
+        board[row_idx][col_idx] = (board[row_idx][col_idx].to_i * 2).to_s
+        board[row_idx][col_idx+1] = " "
+      else
+        next
+      end
+    end
+    strip_row_left(row)
+  end
+  board.transpose
+end
+
+# ---- Main ---- #
+
+new_board = insert_new(game_board)
+
+main_game(new_board)
+
